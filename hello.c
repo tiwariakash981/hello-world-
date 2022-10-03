b754787d17f379aacda016a109423e51b30dd0e7
tiwariakash981 <114892249+tiwariakash981@users.noreply.github.com>
2022-10-03 08:53:12 +0530

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+void main()
+{
+  printf("helloworld");
+}                                                                     (agar maths ka sum rahega to #include<math.h> likhne ka )
